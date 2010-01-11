073695d39a72ed660c3e7dcf70dcf18aebcf4fcb
Perryn <pezcrap@gmail.com>
2010-01-11 16:42:32 +1100

    add compass to env.rb

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "haml", :version => '2.2.16'
+  config.gem "chriseppstein-compass", :version => '0.8.17'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
